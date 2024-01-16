3078f50e828107bc206c1e356b69c893473bbbdd
Assylken Taukenov <79912262+Assylken@users.noreply.github.com>
2024-01-16 23:20:42 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    externalDir: true,
     appDir: true,
     fontLoaders: [
       { loader: "@next/font/google", options: { subsets: ["latin"] } },
     ],
   },
+  disableExperimentalFeaturesWarning: true
 };
 
 module.exports = nextConfig;
