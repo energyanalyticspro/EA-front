6bc559210381de60079eb244feb36c0123a0ac9e
assylken <asylken02@gmail.com>
2024-01-17 21:47:18 +0600

    quick fix

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    externalDir: true,
     appDir: true,
     fontLoaders: [
       { loader: "@next/font/google", options: { subsets: ["latin"] } },
     ],
   },
-  disableExperimentalFeaturesWarning: true
 };
 
 module.exports = nextConfig;
